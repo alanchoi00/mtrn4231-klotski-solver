19bebda8db46f5563cea9e354cd1fa3c9b1256a1
slammyh <samuelharland73@gmail.comh>
2025-11-27 13:46:48 +1100

    fixing launch files

--- src/launch/klotski.launch.py
+++ src/launch/klotski.launch.py
@@ -52,6 +52,10 @@ def generate_launch_description():
         FindPackageShare('pkg_brain'), 'launch', 'brain.launch.py'
     ])
 
+    sense_launch = PathJoinSubstitution([
+        FindPackageShare('pkg_sense'), 'launch', 'sense.launch.py'
+    ])
+
     # UR5e launches
     ur_control_launch = PathJoinSubstitution([
         FindPackageShare('ur_robot_driver'), 'launch', 'ur_control.launch.py'
@@ -156,6 +160,10 @@ def generate_launch_description():
 
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(brain_launch),
+                ),
+
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(sense_launch),
                 ),
 
                 IncludeLaunchDescription(  
