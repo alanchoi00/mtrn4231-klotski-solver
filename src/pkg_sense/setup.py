9aaa5fe053631765bbf7d1f54254e63de5824b1f
slammyh <samuelharland73@gmail.comh>
2025-11-28 16:00:42 +1100

    removing md

--- src/pkg_sense/setup.py
+++ src/pkg_sense/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'HSV_TEST_README.md']),
+        ('share/' + package_name, ['package.xml']),
         (f'share/{package_name}/launch', [
             'launch/sense.launch.py',
             'launch/test_hsv_sense.launch.py',
