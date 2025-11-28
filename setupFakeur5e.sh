9be9e394e3d5bf5ca354706f43af3c518e296cdd
Ram0Gan35h <z5421289@ad.unsw.edu.au>
2025-11-28 16:00:34 +1100

    made it so gripper is in moveit (#8)
    * made it so gripper is shown in moveit
    
    * modified klotski launch.py to have end effector
    
    * fixed collisions in end effector urdf
    
    * deleted un-needed files and directories, renamed visualisation files and packages
    
    * Update setupFakeur5e.sh
    
    ---------
    
    Co-authored-by: Alan Choi <115921805+alanchoi00@users.noreply.github.com>

--- setupFakeur5e.sh
+++ setupFakeur5e.sh
@@ -1,5 +1,13 @@
-gnome-terminal -t "DriverServer" -e 'ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=yyy.yyy.yyy.yyy initial_joint_controller:=joint_trajectory_controller use_fake_hardware:=true launch_rviz:=false '
+gnome-terminal -t "DriverServer" -e 'ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=yyy.yyy.yyy.yyy initial_joint_controller:=scaled_joint_trajectory_controller use_fake_hardware:=true launch_rviz:=false '
 
 sleep 5
 
-gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=ur5e launch_rviz:=true use_fake_hardware:=true'
+gnome-terminal --title="MoveitServer" -- bash -c \
+'ros2 launch ur_moveit_config ur_moveit.launch.py \
+    ur_type:=ur5e \
+    launch_rviz:=true \
+    use_fake_hardware:=true \
+    description_package:=ur_with_gripper_description \
+    description_file:=ur_with_gripper.xacro; \
+    exec bash'
+
